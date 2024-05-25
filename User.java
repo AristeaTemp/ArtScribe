e68646becdf697ce5cb7a2b0d37132a77858d97b
ildahun <hunciilda@gmail.com>
2024-05-25 21:21:03 +0300

    Update User.java

--- User.java
+++ User.java
@@ -1,10 +1,11 @@
+import javax.swing.*;
+
 public class User {
     private String name;
     private String surname;
     private String phoneNumber;
     private String email;
     private String password;
-    
 
     // Constructor
     public User(String name, String surname, String phoneNumber, String email, String password) {
@@ -54,5 +55,27 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    // Method to display user details
+    public void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Surname: " + surname);
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Email: " + email);
+    }
+
+    // Method to change phone number
+    public void changePhoneNumber(String newPhoneNumber) {
+        this.phoneNumber = newPhoneNumber;
+        System.out.println("Phone number changed to: " + newPhoneNumber);
+    }
+
+    public static void main(String[] args) {
+        // Example usage
+        User user = new User("John", "Doe", "1234567890", "john@example.com", "password");
+        user.displayDetails();
+        user.changePhoneNumber("0987654321");
+        user.displayDetails();
     }
 }
