6447fba21ba625f7cbfdd078ef0473b25a6404a8
ildahun <hunciilda@gmail.com>
2024-05-26 18:32:19 +0300

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -9,15 +9,16 @@ public class Menu {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int choice = 0;
-        while (choice != 5) {
+        while (choice != 6) {
             System.out.println("Select an option: ");
             System.out.println("1. Register");
             System.out.println("2. Login");
-            System.out.println("3. Scan QR Code");
-            System.out.println("4. Make Reservation");
-            System.out.println("5. Quit");
+            System.out.println("3. Select City");
+            System.out.println("4. Scan QR Code");
+            System.out.println("5. Make Reservation");
+            System.out.println("6. Quit");
             choice = scanner.nextInt();
-            scanner.nextLine(); // Consume newline
+            scanner.nextLine(); 
             switch (choice) {
                 case 1:
                     registerUser(scanner);
@@ -27,19 +28,26 @@ public class Menu {
                     break;
                 case 3:
                     if (loggedInUser != null) {
-                        scanQRCode(scanner);
+                        selectCity(scanner);
                     } else {
                         System.out.println("Please login first.");
                     }
                     break;
                 case 4:
                     if (loggedInUser != null) {
-                        makeReservation(scanner);
+                        scanQRCode(scanner);
                     } else {
                         System.out.println("Please login first.");
                     }
                     break;
                 case 5:
+                    if (loggedInUser != null) {
+                        makeReservation(scanner);
+                    } else {
+                        System.out.println("Please login first.");
+                    }
+                    break;
+                case 6:
                     System.out.println("Goodbye!");
                     break;
                 default:
@@ -88,6 +96,13 @@ public class Menu {
         }
     }
 
+    static void selectCity(Scanner scanner) {
+        System.out.println("Enter your city:");
+        String city = scanner.nextLine();
+        loggedInUser.setCity(city);
+        System.out.println("City selected: " + city);
+    }
+
     static void scanQRCode(Scanner scanner) {
         System.out.println("Enter the QR code data:");
         String codeData = scanner.nextLine();
@@ -100,7 +115,7 @@ public class Menu {
             SwingWorker<Void, Void> worker = new SwingWorker<>() {
                 @Override
                 protected Void doInBackground() {
-                    // Simulate scanning QR code
+                    
                     ScanQR scanQR = new ScanQR(loggedInUser);
                     scanQR.scanCode(codeData, exhibit);
                     return null;
@@ -136,16 +151,132 @@ public class Menu {
         }
     }
 
-    // This is a stub. In a real application, you would implement this method to retrieve the exhibit from a database.
     static Exhibit getExhibitByNumber(int exhibitNumber) {
-        // Stub: return a dummy exhibit for demonstration purposes
         Museum museum = new Museum("Sample Museum");
         return new Exhibit(exhibitNumber, "Sample Exhibit Description", museum);
     }
 
-    // This is a stub. In a real application, you would implement this method to retrieve the museum from a database.
     static Museum getMuseumByName(String museumName) {
-        // Stub: return a dummy museum for demonstration purposes
         return new Museum(museumName);
+    }
+}
+
+class User {
+    private String name;
+    private String surname;
+    private String phoneNumber;
+    private String email;
+    private String password;
+    private String paymentDetails;
+    private String city;
+
+    public User(String name, String surname, String phoneNumber, String email, String password, String paymentDetails) {
+        this.name = name;
+        this.surname = surname;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.password = password;
+        this.paymentDetails = paymentDetails;
+        this.city = "";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPaymentDetails() {
+        return paymentDetails;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
+
+class ScanQR {
+    private User user;
+
+    public ScanQR(User user) {
+        this.user = user;
+    }
+
+    public void scanCode(String codeData, Exhibit exhibit) {
+        // Simulate scanning QR code
+        System.out.println("User " + user.getName() + " scanned QR code for exhibit " + exhibit.getDescription());
+    }
+}
+
+class Exhibit {
+    private int number;
+    private String description;
+    private Museum museum;
+
+    public Exhibit(int number, String description, Museum museum) {
+        this.number = number;
+        this.description = description;
+        this.museum = museum;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Museum getMuseum() {
+        return museum;
+    }
+}
+
+class Museum {
+    private String name;
+
+    public Museum(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Reservation {
+    private String dateTime;
+    private double price;
+    private Museum museum;
+    private User user;
+
+    public Reservation(String dateTime, double price, Museum museum, User user) {
+        this.dateTime = dateTime;
+        this.price = price;
+        this.museum = museum;
+        this.user = user;
+    }
+
+    public void makeReservation() {
+        // Simulate making a reservation
+        System.out.println("Reservation made for " + user.getName() + " at " + museum.getName() + " on " + dateTime);
     }
 }
