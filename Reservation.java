9bc409eb219addc8205f6dbcf2483a629ec76ca5
ildahun <hunciilda@gmail.com>
2024-06-01 18:35:06 +0300

    Update Reservation.java

--- Reservation.java
+++ Reservation.java
@@ -1,49 +1,84 @@
 package net.codejava;
 
-
+import java.sql.*;
+import javax.swing.*;
 
 public class Reservation {
+    private String name;
+    private String surname;
+    private String phone;
+    private String email;
     private String dateTime;
-    private double price;
+    private double ticketPrice;
     private String museum;
-    private String user;
 
-    public Reservation(String dateTime, double price, String museum, String user) {
+    public Reservation(String name, String surname, String phone, String email, String dateTime, double ticketPrice, String museum) {
+        this.name = name;
+        this.surname = surname;
+        this.phone = phone;
+        this.email = email;
         this.dateTime = dateTime;
-        this.price = price;
+        this.ticketPrice = ticketPrice;
         this.museum = museum;
-        this.user = user;
+    }
+
+    // Getters and setters for other fields
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getDateTime() {
         return dateTime;
     }
 
-    public double getPrice() {
-        return price;
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public double getTicketPrice() {
+        return ticketPrice;
+    }
+
+    public void setTicketPrice(double ticketPrice) {
+        this.ticketPrice = ticketPrice;
     }
 
     public String getMuseum() {
         return museum;
     }
 
-    public String getUser() {
-        return user;
-    }
-
-    public void setDateTime(String dateTime) {
-        this.dateTime = dateTime;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
     public void setMuseum(String museum) {
         this.museum = museum;
     }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
+   
+   
 }
